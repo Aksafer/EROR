9e141ad9b71106073c61659bdcf74c465a12eba9
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-30 19:39:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID", 29303355))
+
+    API_HASH = os.environ.get("API_HASH", "3497383a173bc579a19ae3ca2f8c6114")
+    
+    OWNER = int(os.environ.get("OWNER", 6092147148))
