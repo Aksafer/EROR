5aa719be529a63d82e5f990552aed3c25cad70b5
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-30 18:49:46 +0300

    Add files via upload

---
+++ eror.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Eror(object):
+    ELHYBA = os.environ.get("EROR", "")
+
+    API_ID = int(os.environ.get("API_ID", 29303355))
+
+    API_HASH = os.environ.get("API_HASH", "3497383a173bc579a19ae3ca2f8c6114")
+    
+    OWNER = int(os.environ.get("OWNER", 6092147148))
