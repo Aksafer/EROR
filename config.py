f7175614a1da930d2ac4446f66378d21ea56fc4e
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-30 19:47:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.environ.get("API_ID", 29303355))
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-    API_HASH = os.environ.get("API_HASH", "3497383a173bc579a19ae3ca2f8c6114")
-    
-    OWNER = int(os.environ.get("OWNER", 6092147148))
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID"))
+
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+MUST_JOIN = getenv("MUST_JOIN", None)
